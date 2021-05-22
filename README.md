6f7e3018a08e5f58cc1602463255ed400986c6fc
Paulo César <paulocesarcs.dev@gmail.com>
2021-05-21 21:31:08 -0300

    add class KNN method

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 
-## Euclidean distance
-
- ![formula](https://render.githubusercontent.com/render/math?math=d%20=\sqrt{(%20x_2%20-%20x_1)^2%20%2B%20(%20y_2%20-%20y_1)^2%20})
+- KNN
+#### Euclidean distance
+ #### ![formula](https://render.githubusercontent.com/render/math?math=d%20=\sqrt{(%20x_2%20-%20x_1)^2%20%2B%20(%20y_2%20-%20y_1)^2%20})
---
+++ knn/knn.py
@@ -0,0 +1,20 @@
+import numpy as np
+from collections import Counter
+
+def euclidean_distance(x1, x2):
+    return np.sqrt(np.sum(x1 - x2)**2)
+
+
+class KNN:
+    
+    def __init__(self, k=3):
+        pass
+    
+    def fit(self, X, y):
+        pass
+    
+    def predict(self, X):
+        pass
+    
+    def _predict(self, x):
+        pass
