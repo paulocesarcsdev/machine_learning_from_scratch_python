4f8d375c9f2918ae3ef5d18cedcc3444766dde4b
Paulo César <paulocesarcs.dev@gmail.com>
2021-05-23 19:32:22 -0300

    add class knn on all methods

--- knn/knn.py
+++ knn/knn.py
@@ -8,13 +8,19 @@ def euclidean_distance(x1, x2):
 class KNN:
     
     def __init__(self, k=3):
-        pass
+        self.k = k
     
     def fit(self, X, y):
-        pass
+        self.X_train = X
+        self.y_train = y
     
     def predict(self, X):
-        pass
+        y_pred = [self._predict(x) for x in X]
+        return np.array(y_pred)
     
     def _predict(self, x):
-        pass
+        distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
+        k_idx = np.argsort(distances)[:self.k]
+        k_neighbor_labels = [self.y_train[i] for i in k_idx]
+        most_common = Counter(k_neighbor_labels).most_common(1)
+        return most_common[0][0]
